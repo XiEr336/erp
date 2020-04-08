4f95ca8e23249e7909b8c0035ffb0adc006099c7
lj <3362653125@qq.com>
2020-04-08 16:02:52 +0800

    第一次提交

---
+++ erp-service-impl/src/main/java/cn/csdj/erpserviceimpl/ErpServiceimplApplication.java
@@ -0,0 +1,13 @@
+package cn.csdj.erpserviceimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpServiceimplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpServiceimplApplication.class, args);
+    }
+
+}
